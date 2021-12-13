5c2be7b74e450a2bffc4802e9ec6b5461401babc
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2021-12-13 17:56:02 +0100

    Added return types to functions

--- part1.py
+++ part1.py
@@ -1,11 +1,12 @@
 from random import sample
 from time import sleep
 
-#Grid data Structure. Initial Starting Grid is Randomised. 0 means the square is empty
-grid = sample(range(9),9)
-empty = [i for i in grid if grid[i] == 0][0] #The position of the empty slot
 
-def printGrid(x = False):
+
+def printGrid(grid, x = False):
+    
+    if grid == None: return
+    
     colors = ['','\033[95m', '\033[94m', '\033[96m', '\033[92m', '\033[93m', '\033[91m', '\033[4m', '\033[1m']
     end = '\033[0m'
 
@@ -22,56 +23,78 @@ def printGrid(x = False):
 
     if x: print("\033[%d;%dH" % (0,0))
 
-def moveUp():
-    global empty
-    if empty in [0,1,2]: return -1
+def moveUp(grid):
+    if grid == None: return None
+
+    empty = [i for i in grid if grid[i] == 0][0] #The position of the empty slot
+    if empty in [0,1,2]: return None
     
     grid[empty],grid[empty-3] = grid[empty-3], grid[empty]
     empty -= 3;
-    return 1;
+    return grid
 
-def moveDown():
-    global empty
-    if empty in [6,7,8]: return -1
+def moveDown(grid):
+    if grid == None: return None
+    empty = [i for i in grid if grid[i] == 0][0] #The position of the empty slot
+    if empty in [6,7,8]: return None
 
     grid[empty],grid[empty+3] = grid[empty+3], grid[empty]
     empty += 3
-    return 1
+    return grid
 
-def moveLeft():
-    global empty
-    if empty in [0,3,6]: return -1
+def moveLeft(grid):
+    if grid == None: return None
+    empty = [i for i in grid if grid[i] == 0][0] #The position of the empty slot
+    if empty in [0,3,6]: return None
 
     grid[empty],grid[empty-1] = grid[empty-1], grid[empty]
     empty -= 1
-    return 1
+    return grid
 
-def moveRight():
-    global empty
-    if empty in [2,5,8]: return -1
+def moveRight(grid):
+    if grid == None: return None
+    empty = [i for i in grid if grid[i] == 0][0] #The position of the empty slot
+    if empty in [2,5,8]: return None
 
     grid[empty], grid[empty+1] = grid[empty+1], grid[empty]
     empty += 1
-    return 1
+    return grid
 
-win = lambda: grid == [1,2,3,4,5,6,7,8,0]
+win = lambda grid: grid == [1,2,3,4,5,6,7,8,0]
 
-while True:
+def bfs(grid):
+    #From the current board configuration, take all possible moves (max 4) and check if it is the goal state
+    pass
 
-    dt = 0.75
+def main():
+    #Grid data Structure. Initial Starting Grid is Randomised. 0 means the square is empty
+    # grid = sample(range(9),9)
+    grid = [5,8,7,6,0,4,3,2,1]
+    
+    while not win(grid):
+        
+        dt = 0.75
 
-    moveUp()
-    printGrid(True)
-    sleep(dt)
+        grid = moveUp(grid)
+        printGrid(grid, True)
+        sleep(dt)
 
-    moveRight()
-    printGrid(True)
-    sleep(dt)
+        grid = moveRight(grid)
+        printGrid(grid, True)
+        sleep(dt)
 
-    moveDown()
-    printGrid(True)
-    sleep(dt)
+        grid = moveDown(grid)
+        printGrid(grid, True)
+        sleep(dt)
 
-    moveLeft()
-    printGrid(True)
-    sleep(dt)
+        grid = moveLeft(grid)
+        printGrid(grid, True)
+        sleep(dt)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
